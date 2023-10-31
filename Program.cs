78dab690f8195c6fbf45615ae5ea620b0c4c4ea2
Apk <146819953+7NRVHXY@users.noreply.github.com>
2023-10-31 22:42:59 +0400

    created3

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace elseifnumberofdays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDays = 28;
+            if (numberOfDays == 31)
+            {
+                Console.WriteLine("January");
+            }
+            else if (numberOfDays > 27 && numberOfDays<30)
+            {
+                Console.WriteLine("February");
+            }
+            else if (numberOfDays == 30)
+            {
+                Console.WriteLine("April");
+            }
+            else 
+            {
+                Console.WriteLine("Error!"); 
+            }
+        }
+    }
+}
